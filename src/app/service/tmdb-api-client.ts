2ca07917ac4172e94c74b9ac0d68f380be64adca
nazaninkalhor <nazi.kall@yahoo.com>
2025-06-02 15:14:19 +0330

    Final Version

--- src/app/service/tmdb-api-client.ts
+++ src/app/service/tmdb-api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const api_base_url = process.env.VITE_TMDB_API_BASE_URL;
-const tmdb_api_token = process.env.VITE_TMDB_ACCESS_TOKEN;
+const api_base_url = process.env.NEXT_PUBLIC_TMDB_API_BASE_URL;
+const tmdb_api_token = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN;
 
 
 
